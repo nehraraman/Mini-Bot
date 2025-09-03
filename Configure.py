159d9ecb9bc3aa937216bfd2f9d65a613fd819c7
nehraraman <157287892+nehraraman@users.noreply.github.com>
2025-09-03 13:31:41 +0530

    Delete Configure.py

--- Configure.py
+++
@@ -1,4 +0,0 @@
-# config.py
-
-BOT_TOKEN = "8335379785:AAHK01gK2gaAsF75DoAD0tf-vNPKyJeUq_I"  # apna bot token dalna
-ADMIN_IDS = [123456789, 987654321]  # apna telegram user id dalna (admin ke liye)
